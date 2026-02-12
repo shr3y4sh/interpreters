b835d269806665003eeb736ddc699c48f305970c
shr3y4sh <67783996+shr3y4sh@users.noreply.github.com>
2026-02-13 00:49:25 +0530

    feat scan tokens, addTokens

--- jlox/src/com/interpreter/lox/Scanner.java
+++ jlox/src/com/interpreter/lox/Scanner.java
@@ -6,6 +6,7 @@ import java.util.List;
 import static com.interpreter.lox.TokenType.*;
 
 class Scanner {
+
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
 
@@ -15,5 +16,27 @@ class Scanner {
 
     public Scanner(String source) {
         this.source = source;
+    }
+
+    public List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+
+        return tokens;
+    }
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
     }
 }
